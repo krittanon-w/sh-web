c03bc95132f1d698b0308f10e388821441502cf0
Krittanon.w <krittanon.w@gmail.com>
2017-03-23 09:20:40 +0700

    setup element-ui and babel es2015

--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+
+// setup element-ui env
+// full import components
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-default/index.css'
+Vue.use(ElementUI)
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
